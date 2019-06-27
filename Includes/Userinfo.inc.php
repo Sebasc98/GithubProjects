9e3744008a7759288709b1a0c089d15c4039758d
Sebasc98 <52129238+Sebasc98@users.noreply.github.com>
2019-06-27 13:23:05 +0200

    Add files via upload

--- Includes/Userinfo.inc.php
+++ Includes/Userinfo.inc.php
@@ -1,20 +1,10 @@
 <?php
-// Script die alle informatie van de user uit de database haalt wanneer de user inlogt.
-session_start();
-$sql = "SELECT * FROM inlog 
-INNER JOIN user ON inlog.Userid=user.Userid 
-INNER JOIN adres ON adres.User_Userid=user.Userid
-INNER JOIN bestand ON bestand.User_Userid=user.Userid
-INNER JOIN ticket ON ticket.User_Userid=user.Userid
-INNER JOIN [status] ON [status].User_Userid=user.Userid
-INNER JOIN relationship ON relationship.User_Userid1=user.Userid
-INNER JOIN organisation ON user.Organisation_Organisationid=organisation.Organisationid
-;";
-
-//WERKT
-/*$sql = "SELECT * FROM inlog 
-INNER JOIN user ON inlog.Userid=user.Userid;";*/
-
+// Script die alle informatie van de client uit de db haalt, dus persoonlijke gegevens, bestanden tickets....
+// De Sessievariabele klopt nog niet ik moet uitzoeken hoe je meteen een sessie variabele kan aanmaken nadat er op de link van de client gedrukt is maar
+// voordat de gebruiker naar de pagina verwezen wordt. 
+require 'dbh.inc.php';
+$sql = "SELECT * FROM user LEFT JOIN adres ON user.Userid= adres.User_Userid
+WHERE Userid=?;";
 $stmt=mysqli_stmt_init($conn);
 if(!mysqli_stmt_prepare($stmt, $sql))
         {
@@ -24,7 +14,7 @@ if(!mysqli_stmt_prepare($stmt, $sql))
 else 
 { 
     //placeholder invullen  
-    mysqli_stmt_bind_param($stmt,'ss',$username, $username); // hangt van username variabele af 2x username want inloggen met mail of username
+    mysqli_stmt_bind_param($stmt,'i',$_SESSION['UserID']); // nog veranderen in client id
     mysqli_stmt_execute($stmt);  
     $result = mysqli_stmt_get_result($stmt);
     
@@ -32,9 +22,60 @@ else
     if ($row = mysqli_fetch_assoc($result))
     {
             // alle userinfo 
-            $_SESSION['Username'] = $row['Username'];
-            $_SESSION['Voornaam'] = $row['Firstname'];
-            $_SESSION['Achernaam'] = $row['Lastname'];
-            $_SESSION['Geboortedatum'] = $row['DateOfBirth'];
+            $_SESSION['Client_Voornaam'] = $row['Firstname'];
+            $_SESSION['Client_Initialen'] = $row['Initialen'];
+            if ($row['Tussenvoegsel'] == NULL)
+            {
+                $_SESSION['Client_Tussenvoegsel'] = '';
+            }
+            else 
+            {
+                $_SESSION['Client_Tussenvoegsel'] = $row['Tussenvoegsel'];
+            } 
+            $_SESSION['Client_Achternaam'] = $row['Lastname'];
+            $_SESSION['Client_Geboortedatum'] = $row['DateOfBirth'];
+            $_SESSION['Client_Nationaliteit'] = $row['Nationaliteit'];
+            $_SESSION['Client_Burgerlijkestaat'] = $row['BurgerlijkeStaat'];
+            $_SESSION['Client_telefoonnummer'] = $row['phone'];
+            if ($row['Captive'] == 0)
+            {
+                $_SESSION['Client_Captive'] = 'false';
+            }
+            else 
+            {
+                $_SESSION['Client_Captive'] = 'true';
+            } 
+            $_SESSION['Client_instelling'] = $row['Captive'];
+            $_SESSION['Woonplaats'] = $row['CITY'];
+            $_SESSION['Huisnummer'] = $row['Housenumber'];
+            $_SESSION['Client_straat'] = $row['Street'];
+            $_SESSION['Postcode']= $row['Zipcode'];
+
+            $stmt=mysqli_stmt_init($conn);
+            if(!mysqli_stmt_prepare($stmt, $sql))
+            {
+                echo 'SQL statement failed.';
+            }
+            
+            else 
+            {   // Dit moet nog veranderd worden ik op de clienten pagina staat er een overzicht van relaties de query van mij is fout en moet gefixt worden
+                $sql = "SELECT * FROM relationship LEFT JOIN user ON Relationship.User_Userid1=user.Userid  WHERE User_Userid2=? AND [Type] = Coach;";
+                //placeholder invullen  
+                mysqli_stmt_bind_param($stmt,'i',$_SESSION['UserID']); // nog veranderen in client id
+                mysqli_stmt_execute($stmt);  
+                $result = mysqli_stmt_get_result($stmt);
+        
+                // username en wachtwoord controleren
+                if ($row = mysqli_fetch_assoc($result))
+                {
+                $_SESSION['Client_Coach'] = $row['Firstname'];
+                }
+            }
+            
+            // code voor bestanden tonen
+
+            // code voor script tonen
+            
+            
     }        
 }    
